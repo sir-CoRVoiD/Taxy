e038c01d604ea03f0f8bca1beff6b97c5c2425ed
Sajjad Keshavarz <sajjadkeshavarz@hotmail.com>
2022-04-26 07:45:19 +0430

    f

--- pages/api/tunnel.js
+++ pages/api/tunnel.js
@@ -1,11 +1,16 @@
 export default async function handler(req, res) {
-  if (req.body !== process.env.Pass)
+  if (req.body.pass !== process.env.PASS)
     throw "wrong Password!"
   const [url, method, host, path, headers] = [req.body.url, req.body.method, req.body.host, req.body.path, req.body.headers]
   try {
     const data = await fetch(url, { method, host, path, headers })
-    if (data.status !== "200")
-      throw "status is not 200!"
+    console.log(url);
+    console.log(method);
+    console.log(host);
+    console.log(path);
+    console.log(headers);
+    // if (data.status !== "200")
+    //   throw "status is not 200!"
     res.status(200).json(await data.json())
   } catch (err) {
     res.status(500).json({ msg: err })
