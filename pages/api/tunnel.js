bfc71e0e9d47dd3c0598afe4756a3baa745f8a0a
Sajjad Keshavarz <sajjadkeshavarz@hotmail.com>
2022-04-26 07:30:46 +0430

    Update tunnel.js

--- pages/api/tunnel.js
+++ pages/api/tunnel.js
@@ -1,10 +1,12 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.body !== process.env.Pass)
     throw "wrong Password!"
   const [url, method, host, path, headers] = [req.body.url, req.body.method, req.body.host, req.body.path, req.body.headers]
   try {
     const data = await fetch(url, { method, host, path, headers })
-    res.status(200).json({ data })
+    if (data.status !== "200")
+      throw "status is not 200!"
+    res.status(200).json(await data.json())
   } catch (err) {
     res.status(500).json({ msg: err })
   }
